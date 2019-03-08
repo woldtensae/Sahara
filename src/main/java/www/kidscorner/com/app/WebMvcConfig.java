58a1f91f5c9a6852b086c02c58c51cbcd90ea8f1
Aman Woldtensae <woldtensae.aman@gmail.com>
2019-03-07 23:34:21 -0500

    adding configuration for spring security

---
+++ src/main/java/www/kidscorner/com/app/WebMvcConfig.java
@@ -0,0 +1,50 @@
+package www.kidscorner.com.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebMvcConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private DataSource dataSource;
+	@Value("$.spring.sql.user-query")
+	private String userQuery;
+	@Value("$..spring.sql.role-query")
+	private String roleQuery;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery(userQuery)
+			.authoritiesByUsernameQuery(roleQuery)
+			.dataSource(dataSource)
+			.passwordEncoder(passwordEncoder);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.antMatchers("user/**").hasRole("USER")
+			.antMatchers("admin/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and().csrf().disable()
+			.formLogin()
+			.failureUrl("/failuer")
+			.defaultSuccessUrl("/home")
+			.usernameParameter("username")
+			.passwordParameter("password");
+	}
+	
+	
+}
