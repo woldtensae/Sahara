bc27c37337e61715f15a7d08ea3f3f025ef8105c
Aman Woldtensae <woldtensae.aman@gmail.com>
2019-03-07 22:14:42 -0500

    added service class for role

---
+++ src/main/java/www/kidscorner/com/service/RoleService.java
@@ -0,0 +1,18 @@
+package www.kidscorner.com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import www.kidscorner.com.domain.Role;
+import www.kidscorner.com.repository.RoleRepository;
+
+@Service
+public class RoleService {
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	public Role getRole(String roleType) {
+		return roleRepository.findByRoleType(roleType);
+	}
+}
